6d750c839619c4ce2890d58d328fef1c3cd30e5d
Amit kumar <93227051+Amitkr001@users.noreply.github.com>
2023-08-17 10:17:17 +0530

    Update tree.js

--- projects/file_organizer/module/tree.js
+++ projects/file_organizer/module/tree.js
@@ -1,36 +1,48 @@
+const fs = require('fs');
+const path = require('path');
 
 // Function to display the directory tree
 function treefn(dirPath) {
     if (dirPath === undefined) {
-        treeHelper(process.cwd(), "");
+        // If no directory path is provided, display tree for current working directory
+        treeHelper(process.cwd(), '');
         return;
     } else {
+        // Check if the provided directory path exists
         let doesExist = fs.existsSync(dirPath);
         if (doesExist) {
-            treeHelper(dirPath, "");
+            // Display tree for the provided directory path
+            treeHelper(dirPath, '');
         } else {
-            console.log("Please enter a correct path");
+            console.log('Please enter a correct path');
         }
     }
 }
 
-// Function to display available commands
+// Recursive function to display the directory tree structure
 function treeHelper(dirPath, indent) {
     let isFile = fs.lstatSync(dirPath).isFile();
     if (isFile === true) {
+        // If the path represents a file, display its name
         let fileName = path.basename(dirPath);
-        console.log(indent + "├──" + fileName);
+        console.log(indent + '├──' + fileName);
     } else {
+        // If the path represents a directory, display its name and traverse its children
         let dirName = path.basename(dirPath);
-        console.log(indent + "└──" + dirName);
+        console.log(indent + '└──' + dirName);
+        
+        // Get the list of children (files and directories) within the current directory
         let children = fs.readdirSync(dirPath);
+        
+        // Recursively call treeHelper for each child
         for (let i = 0; i < children.length; i++) {
             let childPath = path.join(dirPath, children[i]);
-            treeHelper(childPath, indent + "\t");
+            // Indentation for subdirectories is increased by a tab
+            treeHelper(childPath, indent + '\t');
         }
     }
 }
 
-module.exports={
-    treekey:treefn
-}
+module.exports = {
+    treekey: treefn
+};
