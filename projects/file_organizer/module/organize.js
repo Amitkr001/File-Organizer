23b94f19fba88741335dc70342c2b8730d27dc8e
Amit kumar <93227051+Amitkr001@users.noreply.github.com>
2023-08-17 09:43:36 +0530

    Update organize.js

--- projects/file_organizer/module/organize.js
+++ projects/file_organizer/module/organize.js
@@ -1,3 +1,10 @@
+const fs = require('fs');
+const path = require("path");
+let types = {
+    media: ["mp4", "mkv"],
+    archives: ['zip', '7z', 'rar', 'tar', 'gz', 'ar', 'iso', "xz"],
+    documents: ['docx', 'doc', 'pdf', 'xlsx', 'xls', 'odt', 'ods', 'odp', 'odg', 'odf', 'txt', 'ps', 'tex'],
+    app: ['exe', 'dmg', 'pkg', "deb"]
 // Function to organize files in a directory
 function organizefn(dirPath) {
     if (dirPath === undefined) {
